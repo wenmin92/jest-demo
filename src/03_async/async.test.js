2ca97c71544385ba02055f328df3de6b45d35b7b
wenmin92 <wenmin92@gmail.com>
2021-07-02 18:47:37 +0800

    add mock

--- src/03_async/async.test.js
+++ src/03_async/async.test.js
@@ -30,54 +30,54 @@ test('the data is "some data" (错误的方式2)', () => {
 });
 
 
-// 方式1: 使用回调参数 done
-test('the data is "some data" (done)', (done) => {
-    fetchData1((data) => {
-        expect(data).toBe("some data2");
-        done();
-    });
-});
+// // 方式1: 使用回调参数 done
+// test('the data is "some data" (done)', (done) => {
+//     fetchData1((data) => {
+//         expect(data).toBe("some data");
+//         done();
+//     });
+// });
 
-// 方式1: 使用回调参数 done (优化)
-test('the data is "some data" (done (优化))', (done) => {
-    fetchData1((data) => {
-        try {
-            expect(data).toBe("some data2");
-            done();
-        } catch (error) {
-            done(error);
-        }
-    });
-});
-
-
-// 方式2: 通过返回 promise
-test('the data is "some data" (返回 promise)', () => {
-    return fetchData2().then((data) => {
-        expect(data).toBe("some data");
-    });
-});
-test('error occur (返回 promise)', () => {
-    expect.assertions(1);
-    return fetchData2(true).catch((err) => {
-        expect(err.toString()).toMatch("fake error");
-    });
-});
-
-// 方式2: 通过返回 promise (简化)
-test('the data is "some data" (返回 promise (简化))', () => {
-    return expect(fetchData2()).resolves.toBe("some data2");
-});
-test('error occur (返回 promise (简化))', () => {
-    return expect(fetchData2(true)).rejects.toThrow("fake error");
-});
+// // 方式1: 使用回调参数 done (优化)
+// test('the data is "some data" (done (优化))', (done) => {
+//     fetchData1((data) => {
+//         try {
+//             expect(data).toBe("some data");
+//             done();
+//         } catch (error) {
+//             done(error);
+//         }
+//     });
+// });
 
 
-// 方式3: 使用 async/await
-test('the data is "some data" (使用 async/await)', async () => {
-    await expect(fetchData2()).resolves.toBe("some data");
-});
+// // 方式2: 通过返回 promise
+// test('the data is "some data" (返回 promise)', () => {
+//     return fetchData2().then((data) => {
+//         expect(data).toBe("some data");
+//     });
+// });
+// test('error occur (返回 promise)', () => {
+//     expect.assertions(1);
+//     return fetchData2(true).catch((err) => {
+//         expect(err.toString()).toMatch("fake error");
+//     });
+// });
 
-test('error occur (使用 async/await)', async () => {
-    await expect(fetchData2(true)).rejects.toThrow("fake error");
-});
+// // 方式2: 通过返回 promise (简化)
+// test('the data is "some data" (返回 promise (简化))', () => {
+//     return expect(fetchData2()).resolves.toBe("some data");
+// });
+// test('error occur (返回 promise (简化))', () => {
+//     return expect(fetchData2(true)).rejects.toThrow("fake error");
+// });
+
+
+// // 方式3: 使用 async/await
+// test('the data is "some data" (使用 async/await)', async () => {
+//     await expect(fetchData2()).resolves.toBe("some data");
+// });
+
+// test('error occur (使用 async/await)', async () => {
+//     await expect(fetchData2(true)).rejects.toThrow("fake error");
+// });
--- src/05_mock/mock3-modules.test.js
+++ src/05_mock/mock3-modules.test.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 import Users from './users';
 
+// axios 对象中的所有方法都会被 mock
 jest.mock('axios');
 
-test('should fetch users', () => {
-    const users = [{name: 'Bob'}];
-    const resp = {data: users};
-    axios.get.mockResolvedValue(resp);
-  
+test('should fetch users', async () => {
+    const users = [{ name: 'Bob' }];
+    const resp = { data: users };
+    axios.get.mockResolvedValue(resp); // get 方法已经被 mock
+
     // or you could use the following depending on your use case:
     // axios.get.mockImplementation(() => Promise.resolve(resp))
-  
-    return Users.all().then(data => expect(data).toEqual(users));
-  });
+
+    await Users.all().then(data => expect(data).toEqual(users));
+    expect(axios.get.mock.calls.length).toBe(1);
+    expect(axios.get.mock.calls[0][0]).toBe('/users.json');
+});
